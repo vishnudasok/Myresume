1aacc17aefdc96ae6e4d02229b3ff54e6abdd423
vishnudasok <vishnudasok2580@gmail.com>
2024-11-21 11:42:20 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', // Update if deploying to a subpath
   plugins: [react()],
- 
   build: {
-    chunkSizeWarningLimit: 1000}
-})
+    assetsInlineLimit: 0,
+    sourcemap: true, // For debugging
+    chunkSizeWarningLimit: 1000, // Increase size limit for chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Separate vendor chunk
+        },
+      },
+    },
+  },
+});
